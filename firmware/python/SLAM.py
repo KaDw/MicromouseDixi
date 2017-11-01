416fbaef69c83699d147b93202fb514b3b2309d6
Karol Trzcinski <k.trzcinski95@gmail.com>
2017-11-01 23:21:30 +0100

    regulator polarny

--- firmware/python/SLAM.py
+++ firmware/python/SLAM.py
@@ -25,8 +25,10 @@ def lqr(A, B, Q, R):
 
 
 class Robot:
+    h = 0.08
+
     class Model:
-        def __init__(self, k=1, zeta=0.64, t_n=None, w_n=None):
+        def __init__(self, k=0.1, zeta=1, t_n=None, w_n=None):
             assert 0 <= zeta <= 1
             self.k = k
             self.zeta = zeta
@@ -97,6 +99,11 @@ class Robot:
                 Xhat = self.X
             U = r-Y  # feedback
             # U = r - self.K*Xhat  # LQR
+            def saturate(x):
+                max_val = 8.0
+                return max_val if x > max_val else -max_val if x < -max_val else x
+            sat = np.vectorize(saturate)
+            #U = sat(U)  # saturate each element
             return U
 
         def measure(self, X):
@@ -114,12 +121,11 @@ class Robot:
             self.pos = np.array([0, 0], dtype=type(0.0))  # x, y
             self.alpha = 0.0
             self.gamma = 1.0
-            self.h = 2
             self.map = np.mat([[]])
 
         def odometry(self, encoders):
-            self.pos += 0.5*(encoders[0, 0] + encoders[1, 0])*np.array([math.cos(self.alpha), -math.sin(self.alpha)])
-            self.alpha += self.gamma * (encoders[0, 0] - encoders[1, 0]) / (2*self.h)
+            self.pos += 0.5*(encoders[0, 0] + encoders[1, 0])*np.array([math.cos(self.alpha), math.sin(self.alpha)])
+            self.alpha += self.gamma * (encoders[1, 0] - encoders[0, 0]) / (2*Robot.h)
 
         def IMU(self, dalpha):
             self.alpha += (1.0-self.gamma) * dalpha
@@ -138,30 +144,33 @@ class Robot:
             self.loc_target_pos = np.array([0, 0])
             self.loc_target_alpha = 0
             self.angles = []
+            self.vel = []
             pass
 
         def _get_delta_to_target(self, current_pos, current_alpha):
             delta_pos = self.loc_target_pos - current_pos
             dist = math.sqrt(sum(delta_pos**2))
-            angle = current_alpha - math.atan2(delta_pos[1], delta_pos[0])
+            angle = math.atan2(delta_pos[1], delta_pos[0]) - current_alpha
+            angle = angle % 2*math.pi
             angle = angle if angle < math.pi else angle - 2*math.pi  # (-pi, pi>
             return dist, angle
 
-        def get_target_vel(self, current_pos, current_alpha):
+        def _get_VW(self, current_pos, current_alpha):
+            k = np.array([1, 1, 1], dtype=float)
             dist, angle = self._get_delta_to_target(current_pos, current_alpha)
-            common = 10*(2/(1+math.exp(-dist)) - 1)  # sigmoid from dist * k
-            common = common*(math.pi - abs(angle))/math.pi  # linear scale to angle
-            diff = 5*angle
-            vl = common + diff
-            vr = common - diff
-            self.angles.append(180/math.pi*angle)
+            v = k[0]*dist*math.cos(angle)
+            omega = 1 if angle == 0 else math.sin(angle)/angle  # sinc angle
+            omega = k[1]*angle + k[0]*math.cos(angle)*omega*(angle + k[2]*self.loc_target_alpha)
+            return v, omega
+
+        def get_target_vel(self, current_pos, current_alpha):
+            v, omega = self._get_VW(current_pos, current_alpha)
+            vl = v - omega*Robot.h
+            vr = v + omega*Robot.h
+            self.angles.append(omega)
+            self.vel.append(v)
             return np.mat([[vl],
                            [vr]])
-            self.t += 1
-            return np.mat([
-                    [10],
-                    [math.sin(self.t)]
-                ])
 
     def __init__(self):
         self.model = Robot.Model()
@@ -172,7 +181,9 @@ class Robot:
     def iterate(self):
         pos = self.slam.pos
         r = self.profiler.get_target_vel(self.slam.pos, self.slam.alpha)
-        r = np.mat([[10], [11]])
+        #r = np.mat([[14], [13]])
+        #if len(self.profiler.angles) > 100:
+        #    r = np.mat([[13], [14]])
         self.model.iterate(r, 0.001)
         self.slam.odometry(self.model.Y)
         return pos
@@ -182,14 +193,22 @@ def trace_to(target, robot):
     alpha = []
     robot.profiler.loc_target_pos = target[0:2]
     robot.profiler.loc_target_alpha = target[2]
-    for i in range(200):
+    for i in range(900):
         robot.iterate()
         pos.append(robot.slam.pos.tolist())
         alpha.append(robot.slam.alpha)
     return pos, alpha
 
-model = Robot.Model(t_n=2)
+model = Robot.Model(t_n=0.8, k=0.1)
 mm = Robot()
+
+'''
+f = open('2pwm250.txt')
+p = []
+for l in f.readlines()[3:]:
+    p.append(int(l.split()[0]))
+plt.plot(p)
+plt.show()'''
 
 tar = (100, 50, 1)
 pos, angle = trace_to(tar, mm)
@@ -202,6 +221,7 @@ for (x, y), angle in zip(pos, angle):
 plt.axis('equal')
 plt.figure()
 plt.plot(mm.profiler.angles)
+plt.plot(mm.profiler.vel)
 plt.show()
 
 
